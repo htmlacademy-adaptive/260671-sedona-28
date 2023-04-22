52c4372a4bd278d2500d7d5d2a06b53c7d048a3a
sergentors705 <sergentors705@gmail.com>
2023-04-22 19:31:28 +0600

    added statuses and icon toggle to main menu

--- source/js/menu-toggle.js
+++ source/js/menu-toggle.js
@@ -1,13 +1,22 @@
 let menu = document.querySelector('.page-header__menu');
 let btn = document.querySelector('.menu__toggle');
+let text = document.querySelector('.menu__toggle--text');
+let menuOpenIcon = document.querySelector('.menu__icon--open');
+let menuCloseIcon = document.querySelector('.menu__icon--close');
 
 menu.classList.remove('page-header__menu--nojs');
 
 btn.addEventListener('click', function () {
   if (menu.classList.contains('page-header__menu--closed')) {
+    text.textContent = "Закрыть меню.";
+    menuOpenIcon.style.display = "none";
+    menuCloseIcon.style.display = "block";
     menu.classList.remove('page-header__menu--closed');
     menu.classList.add('page-header__menu--opened');
   } else {
+    text.textContent = "Открыть меню.";
+    menuOpenIcon.style.display = "block";
+    menuCloseIcon.style.display = "none";
     menu.classList.add('page-header__menu--closed');
     menu.classList.remove('page-header__menu--opened');
   }
